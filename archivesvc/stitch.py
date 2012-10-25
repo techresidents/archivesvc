a611a54b345aee628368ba485c59681342869c83
Jeffrey Mullins <jmullins@techresidents.com>
2012-10-25 13:59:13 -0400

    Fixing bug where single participant archives are not stitched properly.

--- archivesvc/stitch.py
+++ archivesvc/stitch.py
@@ -381,7 +381,7 @@ class FFMpegSoxStitcher(ArchiveStitcher):
                             (stream.offset or 0)/1000.0))
                 sox_arguments.append(output_path)
             else:
-                input_filename = archive_streams[0].filename
+                input_filename = storage_backend.path(archive_streams[0].filename)
                 sox_arguments = [
                         self.sox_path,
                         "--norm",
